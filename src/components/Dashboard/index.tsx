9e3c12bb49457a3288493f3812b9e289ed42ec53
Gustavo <eugumatos@Gustavos-MacBook-Air.local>
2021-03-15 21:53:29 -0300

    first commit

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,12 @@
+import { Container } from './styles';
+import { Sumary } from '../Sumary';
+import { TransactionsTable } from '../TransactionsTable';
+
+export function Dashboard() {
+  return (
+    <Container>
+      <Sumary />
+      <TransactionsTable />
+    </Container>
+  );
+}
