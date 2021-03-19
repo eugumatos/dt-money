b09e9f97162fe6c01ff76a41742f0a4054f6678b
Gustavo <eugumatos@Gustavos-MacBook-Air.local>
2021-03-18 23:47:27 -0300

    Create modal and update route mirageJS

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
+import React, { useState } from 'react';
+import Modal from 'react-modal';
+
 import { Header } from './components/Header';
 import { Dashboard } from './components/Dashboard';
+import { NewTransactionModal } from './components/NewTransactionModal';
+
 import { GlobalStyle } from './styles/global';
 
+Modal.setAppElement('#root');
+
 export function App() {
+  const [isNewTrasactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactonModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactonModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactonModal}/>
         <Dashboard />
+        <NewTransactionModal 
+          isOpen={isNewTrasactionModalOpen} 
+          onRequestClose={handleCloseNewTransactonModal} 
+        />
       <GlobalStyle />
     </>
   );
